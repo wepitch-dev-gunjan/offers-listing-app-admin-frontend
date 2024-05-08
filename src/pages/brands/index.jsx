846ffb9444f40acdbe455097b8ecbb57795b5d73
Nikhil mandia <nikhilmandia7@gmail.com>
2024-05-08 11:40:22 +0530

    adfsdf

--- src/pages/brands/index.jsx
+++ src/pages/brands/index.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.scss";
 import { CiShare1 } from "react-icons/ci";
+import axios from "axios";
 const Brands = ({ addBrandClicked, setAddBrandClicked }) => {
+  const [brand, setBrand] = useState([]);
   let img =
     "https://img.freepik.com/free-vector/sale-offer-label-banner-discount-offer-promotion_157027-1250.jpg";
   let para =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus,harum id provident quos voluptatem delectus optio perferendisplaceat dicta facere ducimus deserunt animi repudiandae laboreautem enim? Numquam, soluta officia?";
-  const data = [
-    { id: 1, img: img, heading: "Offer Heading", para: para },
-    { id: 2, img: img, heading: "Offer Heading", para: para },
-    { id: 3, img: img, heading: "Offer Heading", para: para },
-    { id: 4, img: img, heading: "Offer Heading", para: para },
-    { id: 5, img: img, heading: "Offer Heading", para: para },
-    { id: 6, img: img, heading: "Offer Heading", para: para },
-  ];
+
+  const getOffers = async () => {
+    const { data } = await axios.get(`http://localhost:8080/brand`);
+    setBrand(data);
+  };
+  useEffect(() => {
+    getOffers();
+  }, []);
+  console.log(brand);
   return (
     <div className="Brands-container">
       <div className="add-brands-btn">
@@ -22,16 +25,16 @@ const Brands = ({ addBrandClicked, setAddBrandClicked }) => {
         </div>
       </div>
       <div className="brands">
-        {data.map((data, i) => {
+        {brand.map((data, i) => {
           return (
             <div className="col">
               <div className="brand">
                 <div className="brand-image">
-                  <img src={data.img} alt="brand image" />
+                  <img src={data.logo} alt="brand image" />
                 </div>
                 <div className="brand-details">
-                  <h3>{data.heading}</h3>
-                  <p>{data.para}</p>
+                  <h3>{data.title}</h3>
+                  <p>{data.description}</p>
                   <div className="brand-view">
                     View brand page
                     <CiShare1 fontWeight={400} />
